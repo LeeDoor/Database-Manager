896f05277e8179f85a35a3ffc594b954c95659c3
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-08-25 19:34:41 +0300

    Order List

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -24,12 +24,30 @@ namespace Database_Manager
             }
         }
 
+
+        private Order _selectedOrder;
+        public ObservableCollection<Order> Orders { get; set; }
+        public Order SelectedOrder
+        {
+            get => _selectedOrder;
+            set
+            {
+                _selectedOrder = value;
+                OnPropertyChanged(nameof(SelectedOrder));
+            }
+        }
         public MainWindowViewModel()
         {
             Users = new ObservableCollection<User>()
             {
                 new User {Id = 0, Name = "Oleg", Phone = 22555 },
                 new User {Id = 1, Name = "Sergay", Phone = 44455 }
+            };
+
+            Orders = new ObservableCollection<Order>()
+            {
+                new Order{ Id = 0, CustomerId = 0, Summ = 150.0m, Date = "2000-12-12" },
+                new Order{ Id = 1, CustomerId = 0, Summ = 2000.0m, Date = "2000-12-13" }
             };
         }
 
--- Order.cs
+++ Order.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Database_Manager
 {
-    public class Order
+    public class Order : INotifyPropertyChanged
     {
+        private int id;
+        private int customerId;
+        private decimal summ;
+        private string date;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public int CustomerId
+        {
+            get => customerId;
+            set
+            {
+                customerId = value;
+                OnPropertyChanged(nameof(CustomerId));
+            }
+        }
+        public decimal Summ
+        {
+            get => summ;
+            set
+            {
+                summ = value;
+                OnPropertyChanged(nameof(Summ));
+            }
+        }
+        public string Date
+        {
+            get => date;
+            set
+            {
+                date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

5a55e99e65eb6d66d9fb271d39a15c91be05750b
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-08-25 21:14:28 +0300

    mod buttons
    not working

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -11,7 +12,8 @@ namespace Database_Manager
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-        
+
+        #region grids
         private User _selectedUser;
         public ObservableCollection<User> Users { get; set; }
         public User SelectedUser
@@ -36,6 +38,39 @@ namespace Database_Manager
                 OnPropertyChanged(nameof(SelectedOrder));
             }
         }
+        #endregion
+
+        public DelegateCommand AddPersonClick { get; set; }
+        public DelegateCommand EditPersonClick { get; set; }
+        public DelegateCommand DeletePersonClick { get; set; }
+        public DelegateCommand AddOrderClick { get; set; }
+        public DelegateCommand DeleteOrderClick { get; set; }
+
+        public async void AddPerson()
+        {
+
+        }
+        public async void EditPerson()
+        {
+
+        }
+        public async void DeletePerson()
+        {
+
+        }
+
+        public async void AddOrder()
+        {
+
+        }
+        public async void DeleteOrder()
+        {
+
+        }
+
+        public bool IsPersonSelected() => SelectedUser != null;
+        public bool IsOrderSelected() => SelectedOrder != null;
+
         public MainWindowViewModel()
         {
             Users = new ObservableCollection<User>()
@@ -49,6 +84,13 @@ namespace Database_Manager
                 new Order{ Id = 0, CustomerId = 0, Summ = 150.0m, Date = "2000-12-12" },
                 new Order{ Id = 1, CustomerId = 0, Summ = 2000.0m, Date = "2000-12-13" }
             };
+            //сдесь observes can execute не работает по непонятной причине, и кнопка не становится видимой когда появляется выбранный элемент
+            AddPersonClick = new DelegateCommand(AddPerson);//, () => true);//.ObservesCanExecute(() => true);
+            EditPersonClick = new DelegateCommand(EditPerson);//, IsPersonSelected);//.ObservesCanExecute(IsPersonSelected);
+            DeletePersonClick = new DelegateCommand(DeletePerson);//, IsPersonSelected);//.ObservesCanExecute(IsPersonSelected);
+
+            AddOrderClick = new DelegateCommand(AddOrder);//, () => true);//.ObservesCanExecute(() => true);
+            DeleteOrderClick = new DelegateCommand(DeleteOrder);//, IsOrderSelected);//.ObservesCanExecute(IsOrderSelected);
         }
 
         public void OnPropertyChanged(string prop)

cc9cd2ba0c62fca851d4307bf8d13b066b04c6eb
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-09-01 18:19:57 +0300

    now we save connection

--- Model/DatabaseManager.cs
+++ Model/DatabaseManager.cs
@@ -12,7 +12,7 @@ namespace Database_Manager.Model
 {
     public static class DatabaseManager
     {
-        private static readonly string connectionStr;
+        private static readonly SqlConnection connection;
 
         static DatabaseManager()
         {
@@ -20,67 +20,55 @@ namespace Database_Manager.Model
             //dialog.ShowDialog();
             //string connectionStr = dialog.ConnectionString;
 
-            connectionStr = "Server=localhost\\SQLEXPRESS;Trusted_Connection=True;Encrypt=False;Database=ShopDB;";// MultipleActiveResultSets=True;";
-
+            string connectionStr = "Server=localhost\\SQLEXPRESS;Trusted_Connection=True;Encrypt=False;Database=ShopDB;";
+            connection = new SqlConnection(connectionStr);
+            connection.Open();
         }
 
         public static void ClearDb()
         {
-            using (SqlConnection connection = new SqlConnection(connectionStr))
-            {
-                connection.Open();
-                SqlCommand command = new SqlCommand($"DELETE FROM Orders", connection);
-                command.ExecuteNonQuery();
+            SqlCommand command = new SqlCommand($"DELETE FROM Orders", connection);
+            command.ExecuteNonQuery();
 
-                command = new SqlCommand($"DELETE FROM Users", connection);
-                command.ExecuteNonQuery();
-            }
+            command = new SqlCommand($"DELETE FROM Users", connection);
+            command.ExecuteNonQuery();
         }
 
         public static ObservableCollection<User> GetUsers()
         {
-            using (SqlConnection connection = new SqlConnection(connectionStr))
+            SqlCommand command = new SqlCommand("SELECT * FROM Users", connection);
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                connection.Open();
+                ObservableCollection<User> users = new ObservableCollection<User>();
 
-                SqlCommand command = new SqlCommand("SELECT * FROM Users", connection);
-                using (SqlDataReader reader = command.ExecuteReader())
+                while (reader.Read())
                 {
-                    ObservableCollection<User> users = new ObservableCollection<User>();
+                    object id = reader.GetValue(0);
+                    object name = reader.GetValue(1);
+                    object phone = reader.GetValue(2);
 
-                    while (reader.Read())
-                    {
-                        object id = reader.GetValue(0);
-                        object name = reader.GetValue(1);
-                        object phone = reader.GetValue(2);
-
-                        users.Add(new User() { Id = (int)id, Name = name.ToString() ?? "", Phone = (int)phone });
-                    }
-                    return users;
+                    users.Add(new User() { Id = (int)id, Name = name.ToString() ?? "", Phone = (int)phone });
                 }
+                return users;
             }
+
         }
         public static ObservableCollection<Order> GetOrders()
         {
-            using (SqlConnection connection = new SqlConnection(connectionStr))
+            SqlCommand command = new SqlCommand("SELECT * FROM Orders", connection);
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                connection.Open();
+                ObservableCollection<Order> orders = new ObservableCollection<Order>();
 
-                SqlCommand command = new SqlCommand("SELECT * FROM Orders", connection);
-                using (SqlDataReader reader = command.ExecuteReader())
+                while (reader.Read())
                 {
-                    ObservableCollection<Order> orders = new ObservableCollection<Order>();
-
-                    while (reader.Read())
-                    {
-                        object id = reader.GetValue(0);
-                        object customerId = reader.GetValue(1);
-                        object sum = reader.GetValue(2);
-                        object date = reader.GetValue(3);
-                        orders.Add(new Order() { Id = (int)id, CustomerId = (int)customerId, Summ = (decimal)sum, Date = DateTime.Parse(date.ToString() ?? "2000-12-12") });
-                    }
-                    return orders;
+                    object id = reader.GetValue(0);
+                    object customerId = reader.GetValue(1);
+                    object sum = reader.GetValue(2);
+                    object date = reader.GetValue(3);
+                    orders.Add(new Order() { Id = (int)id, CustomerId = (int)customerId, Summ = (decimal)sum, Date = DateTime.Parse(date.ToString() ?? "2000-12-12") });
                 }
+                return orders;
             }
         }
 
@@ -104,13 +92,11 @@ namespace Database_Manager.Model
             }
             sb.Append(";");
 
-            using (SqlConnection connection = new SqlConnection(connectionStr))
-            {
-                connection.Open();
-                SqlCommand command = new SqlCommand(sb.ToString(), connection);
-                return command.ExecuteNonQuery();
-            }
+            SqlCommand command = new SqlCommand(sb.ToString(), connection);
+            return command.ExecuteNonQuery();
+
         }
+
         private static int FillDefaultValuesOrders()
         {
             Random random = new Random();
@@ -125,64 +111,49 @@ namespace Database_Manager.Model
             }
             sb.Append(";");
 
-            using (SqlConnection connection = new SqlConnection(connectionStr))
-            {
-                connection.Open();
-                SqlCommand command = new SqlCommand(sb.ToString(), connection);
-                return command.ExecuteNonQuery();
-            }
+            SqlCommand command = new SqlCommand(sb.ToString(), connection);
+            return command.ExecuteNonQuery();
+
         }
 
         public static int AddUser(string name, int phone)
         {
-            using (SqlConnection connection = new SqlConnection(connectionStr))
-            {
-                connection.Open();
+            SqlCommand command = new SqlCommand("INSERT INTO Users VALUES (@name, @phone);", connection);
 
-                SqlCommand command = new SqlCommand("INSERT INTO Users VALUES (@name, @phone);", connection);
+            SqlParameter paramName = new SqlParameter("@name", name);
+            command.Parameters.Add(paramName);
 
-                SqlParameter paramName = new SqlParameter("@name", name);
-                command.Parameters.Add(paramName);
+            SqlParameter paramPhone = new SqlParameter("@phone", phone);
+            command.Parameters.Add(paramPhone);
 
-                SqlParameter paramPhone = new SqlParameter("@phone", phone);
-                command.Parameters.Add(paramPhone);
+            return command.ExecuteNonQuery();
 
-                return command.ExecuteNonQuery();
-            }
         }
         public static int EditUser(int idToEdit, string newname, int newphone)
         {
-            using (SqlConnection connection = new SqlConnection(connectionStr))
-            {
-                connection.Open();
+            SqlCommand command = new SqlCommand("UPDATE Users SET Name = @name, Phone = @phone WHERE Id = @idToChange", connection);
 
-                SqlCommand command = new SqlCommand("UPDATE Users SET Name = @name, Phone = @phone WHERE Id = @idToChange", connection);
+            SqlParameter paramName = new SqlParameter("@name", newname);
+            command.Parameters.Add(paramName);
 
-                SqlParameter paramName = new SqlParameter("@name", newname);
-                command.Parameters.Add(paramName);
+            SqlParameter paramPhone = new SqlParameter("@phone", newphone);
+            command.Parameters.Add(paramPhone);
 
-                SqlParameter paramPhone = new SqlParameter("@phone", newphone);
-                command.Parameters.Add(paramPhone);
+            SqlParameter paramId = new SqlParameter("@idToChange", idToEdit);
+            command.Parameters.Add(paramId);
 
-                SqlParameter paramId = new SqlParameter("@idToChange", idToEdit);
-                command.Parameters.Add(paramId);
+            return command.ExecuteNonQuery();
 
-                return command.ExecuteNonQuery();
-            }
         }
         public static int RemoveUser(int idToRemove)
         {
-            using (SqlConnection connection = new SqlConnection(connectionStr))
-            {
-                connection.Open();
+            SqlCommand command = new SqlCommand("DELETE FROM Users WHERE Id = @idToChange", connection);
 
-                SqlCommand command = new SqlCommand("DELETE FROM Users WHERE Id = @idToChange", connection);
+            SqlParameter paramId = new SqlParameter("@idToChange", idToRemove);
+            command.Parameters.Add(paramId);
 
-                SqlParameter paramId = new SqlParameter("@idToChange", idToRemove);
-                command.Parameters.Add(paramId);
+            return command.ExecuteNonQuery();
 
-                return command.ExecuteNonQuery();
-            }
         }
     }
 }
